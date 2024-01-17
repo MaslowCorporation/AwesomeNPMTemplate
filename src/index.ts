d54b815ff19da90b854960843529fda30b2b8f31
MaslowCorporation <maslow.gpt.corp.97200@gmail.com>
2024-01-17 01:58:11 +0100

    First commit ;-)

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// Your Own SDK Methods imports
+/* PLOP_INJECT_IMPORT */
+
+// Starterpack SDK Methods imports
+import GetWorkStatus from "./services/GetWorkStatus/GetWorkStatus.js";
+import GetAPISubscriptionLink from "./services/GetAPISubscriptionLink/GetAPISubscriptionLink.js";
+import Buy5KAPICreditsLink from "./services/Buy5KAPICreditsLink/Buy5KAPICreditsLink.js";
+import GetAPIClientInfo from "./services/GetAPIClientInfo/GetAPIClientInfo.js";
+import UpdateWorkData from "./services/UpdateWorkData/UpdateWorkData.js";
+
+export default {
+  // Your Own SDK Methods
+  /* PLOP_INJECT_SDK_METHOD */
+
+  // Starterpack SDK Methods
+  GetAPISubscriptionLink,
+  Buy5KAPICreditsLink,
+  GetAPIClientInfo,
+  GetWorkStatus,
+  UpdateWorkData,
+};
---
+++ tests/index.spec.ts
@@ -0,0 +1,16 @@
+import "mocha";
+import { assert } from "chai";
+
+describe("GetChatGPTOutput", () => {
+  it("should be a function", () => {
+
+  });
+
+  it("Here's the test playground", () => {
+    /*const expected = "Goodbye from my example modern npm package!";
+    const actual = goodBye();
+    assert.equal(actual, expected);*/
+
+
+  });
+});
